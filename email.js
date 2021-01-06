7b38318bcecdf63cca6c02f7a020c2b4c743512b
Danilo <daniloalalmeida@gmail.com>
2021-01-06 19:57:02 -0300

    commit emailJS funcionou

--- email.js
+++ email.js
@@ -1,29 +1,27 @@
-const express = require('express')
-const nodemailer = require('nodemailer')
-const app = express()
-
-const port = 3000
+const nodemailer = require('nodemailer');
 
 const user = "contato@importlink.com.br";
 const pass = " ";
 
-app.get('/send',(req, res) => {
-
-    const transporter = nodemailer.createTransport({
-        host: "mail.importlink.com.br",
-        port: 587,
+let transporter = nodemailer.createTransport({
+        host: "importlink.com.br",
+        port: 465,
+        secure: true,
         auth: {user, pass}
-    })
+    });
+
 
     transporter.sendMail({
         from: user,
-        to: "contato@importlink.com.br",
-        replyTo: "daniloalalmeida@outlook.com.br",
-        subject: "Obrigado por seu e-mail",
-        text: "Obrigado por seu e-mail, entraremos em contato",
-    }).then(info=>{
-        req.send(info)
-    }).catch(error =>{
-        res.send(error)
+        to: "telmo.almeida@importlink.com.br",
+        replyTo: "email",
+        subject: "Contato via site",
+        text: "Telmo, esta pessoa abaixo fez contato com você fica site. A mensagem dela é: ",
+    }).then(message => {
+        console.log(message);
+    }).catch(err => {
+        console.log(err);
     })
-})
+    
+
+
